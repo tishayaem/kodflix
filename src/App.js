25c64557b2b87f019a50f654ed29bbd1d39ade54
tishayaem <kostiantynkalinin@gmail.com>
2018-07-02 11:14:32 +0100

    Added movie poster #1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import samuraiCover from './images/samurai_cover.jpg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
           <h1 className="App-title">Welcome to Kodflix</h1>
         </header>
-        <p className="App-intro">
-
-          To get started, edit <code>src/App.js</code> and save to reload.
-        Test
+        <p className="Poster">
+        <img src={samuraiCover} className="main-poster"/>
         </p>
       </div>
     );
