13e80d20455b9f3ed54f91d5c164d7a6819e8853
tishayaem <kostiantynkalinin@gmail.com>
2018-07-03 10:58:41 +0100

    Added six shows covers #2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import samuraiCover from './images/samurai_cover.jpg';
+import basketsCover from './images/baskets-poster.jpg';
+import twinPeaksCover from './images/twin-peaks.jpg';
+import gameOfThronesCover from './images/gameOfThrones-poster.jpg';
+import topOfTheLakeCover from './images/top-of-the-lake-poster.jpg';
+import winterSunCover from './images/winter-sun-poster.jpg';
+import youngPopeCover from './images/young-pope-cover.jpg';
 
 class App extends Component {
   render() {
@@ -11,9 +16,34 @@ class App extends Component {
           
           <h1 className="App-title">Welcome to Kodflix</h1>
         </header>
-        <p className="Poster">
-        <img src={samuraiCover} className="main-poster"/>
-        </p>
+        {/* <p className="Poster">
+        <img src={samuraiCover} className="main-poster" alt=" "/>
+        </p> */}
+        <div className="Titles">
+          <div className="flex-container1">
+            <h1 className="flex-item">
+              <img src={basketsCover} alt=" "/>
+            </h1>
+            <h1 className="flex-item">
+              <img src={twinPeaksCover} alt=" "/>
+            </h1>
+            <h1 className="flex-item">
+              <img src={gameOfThronesCover} alt=" "/>
+            </h1>
+          </div>
+          <div className="flex-container2">
+            <h1 className="flex-item">
+              <img src={topOfTheLakeCover} alt=" "/>
+            </h1>
+            <h1 className="flex-item">
+              <img src={winterSunCover} alt=" "/>
+            </h1>
+            <h1 className="flex-item">
+              <img src={youngPopeCover} alt=" "/>
+            </h1>
+          </div>
+        </div>
+
       </div>
     );
   }
