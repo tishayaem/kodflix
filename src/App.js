0989a8c677d25d3ef2340c76a6a698bb0875f99e
tishayaem <kostiantynkalinin@gmail.com>
2018-07-05 09:12:13 +0100

    Animated the TV show title overlay(fix)#6

--- src/App.js
+++ src/App.js
@@ -7,68 +7,65 @@ import gameOfThronesCover from './images/gameOfThrones-poster.jpg';
 import topOfTheLakeCover from './images/top-of-the-lake-poster.jpg';
 import winterSunCover from './images/winter-sun-poster.jpg';
 import youngPopeCover from './images/young-pope-cover.jpg';
+import Cover from './'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          
           <h1 className="App-title">Welcome to Kodflix</h1>
         </header>
-        {/* <p className="Poster">
-        <img src={samuraiCover} className="main-poster" alt=" "/>
-        </p> */}
         <div className="Titles">
-          
+
           <div className="container1">
-            
+
             <div className="item">
-              <img src={basketsCover} alt=" "/>
+              <img src={basketsCover} alt=" " />
               <div className="item-overlay">
                 <h1>Baskets</h1>
               </div>
             </div>
-            
+
             <div className="item">
-              <img src={twinPeaksCover} alt=" "/>
+              <img src={twinPeaksCover} alt=" " />
               <div className="item-overlay">
                 <h1>Twin Peaks</h1>
               </div>
             </div>
-            
+
             <div className="item">
-              <img src={gameOfThronesCover} alt=" "/>
+              <img src={gameOfThronesCover} alt=" " />
               <div className="item-overlay">
                 <h1>Game of Thrones</h1>
               </div>
             </div>
 
           </div>
-          
+
           <div className="container2">
-            
+
             <div className="item">
-              <img src={topOfTheLakeCover} alt=" "/>
+              <img src={topOfTheLakeCover} alt=" " />
               <div className="item-overlay">
                 <h1>Top of the Lake</h1>
               </div>
             </div>
-            
+
             <div className="item">
-              <img src={winterSunCover} alt=" "/>
+              <img src={winterSunCover} alt=" " />
               <div className="item-overlay">
                 <h1>Low Winter Sun</h1>
               </div>
             </div>
-            
+
             <div className="item">
-              <img src={youngPopeCover} alt=" "/>
+              <img src={youngPopeCover} alt=" " />
               <div className="item-overlay">
                 <h1>Young Pope</h1>
               </div>
             </div>
-         
+
           </div>
         </div>
 
