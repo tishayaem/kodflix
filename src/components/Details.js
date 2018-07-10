719f56f3814e23ff9dff4493c2f39e2465141fe8
Constantin Kalinin <kostiantynkalinin@gmail.com>
2018-07-10 11:25:12 +0100

    Introduced component state #13 (#14)
    * Introduced component state #13
    
    * Used SetTimeout;Removed unnecessary variable #13
    
    * Applied reviewer suggestions #13
    
    * renamed function

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
 export default class Details extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { message: 'Hello, this will be the details page for each Movie & TV show' };
+    }
+
+    componentDidMount() {
+        setTimeout(
+            () => this.setComingSoonMessage(),
+            3000
+        );
+    }
+
+    setComingSoonMessage() {
+        this.setState({
+            message: 'Coming Soon!'
+        });
+    }
+
     render() {
-        return(
-        <h2>Details</h2>
-        )
+        return (
+            <h2>{this.state.message}</h2>
+        );
     }
 }
+
+
+
