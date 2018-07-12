30c2640dbe903cb71a4b2f11d6e4b2ab4e418b69
tishayaem <kostiantynkalinin@gmail.com>
2018-07-12 10:03:10 +0100

    Returned a list of shows from the backend #20

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const getListOfShows = require('./list')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/rest/shows', (req, res) => res.send(getListOfShows()))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
