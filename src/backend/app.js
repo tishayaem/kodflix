92f21e2a1fdf304d8a68cf807a007fb687b3408d
tishayaem <kostiantynkalinin@gmail.com>
2018-07-13 11:11:46 +0100

    Setup for enabling backend server in production

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const getListOfShows = require('./list')
+
+app.use(express.static(path.join(__dirname, '../../build')));
 
 app.get('/rest/shows', (req, res) => res.send(getListOfShows()))
 
-app.listen(3001, () => console.log('Example app listening on port 3000!'))
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
+
+
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => console.log('Example app listening on port 3001!'))
