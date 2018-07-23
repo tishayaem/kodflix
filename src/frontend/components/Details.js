7af44e54ffad6fa482f22aa8aa4d3ef25f4f7b9c
tishayaem <kostiantynkalinin@gmail.com>
2018-07-23 16:42:45 +0100

    Added loading indicator

--- src/frontend/components/Details.js
+++ src/frontend/components/Details.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { movieDB } from "../tvShowDatabase";
 import { Redirect } from "react-router-dom";
-
+import Loading from "./Loading/Loading"
 export default class Details extends React.Component {
   constructor() {
     super();
@@ -19,7 +19,7 @@ export default class Details extends React.Component {
 
   render() {
     if (Object.keys(this.state.movie).length === 0) {
-      return <div> Loading </div>;
+      return <div> <Loading /> </div>;
     } else if (this.state.movie === undefined) {
       return <Redirect to="/notfound" />;
     } else {
