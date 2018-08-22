a172c913858ff7b29d447d05a542e26794b336aa
tishayaem <kostiantynkalinin@gmail.com>
2018-08-22 23:14:53 +0100

    Add support for Google Analytics! #34

--- src/backend/db.js
+++ src/backend/db.js
@@ -6,7 +6,7 @@ console.log(process.env.NODE_ENV)
 module.exports = function connectDb() {
   return MongoClient.connect(url)
     .then(function(db) {
-      const dbo = db.db("kodflix");
+      const dbo = db.db("kodflixDB");
       return dbo;
     })
     .catch(function(err) {});
